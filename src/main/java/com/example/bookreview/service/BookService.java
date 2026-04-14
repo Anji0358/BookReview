07c9e168ff1b5a74bce359ceec4e4f8537506219
Anji <zam58mrp@i.softbank.jp>
2026-04-14 19:12:53 +0900

    check:キャッシュが機能しているか確認

--- src/main/java/com/example/bookreview/service/BookService.java
+++ src/main/java/com/example/bookreview/service/BookService.java
@@ -18,53 +18,79 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class BookService {
-	
+
 	@Autowired
 	private BookRepository bookRepository;
-	
-	
+
 	/**
-     * 全ての書籍を取得します。
-     */
+	 * 全ての書籍を取得します。
+	 */
 	@Transactional
 	@Cacheable("getBooks")
-	public List<Book> findAll(){
-		
-		List<Book> allBooks=new ArrayList<>();
-		
-		bookRepository.findAll().forEach(allBooks::add);;
+	public List<Book> findAll() {
+
+		List<Book> allBooks = new ArrayList<>();
+
+		//キャッシュが機能しているかの確認
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+
+		bookRepository.findAll().forEach(allBooks::add);
+		;
 		return allBooks;
 	}
 
 	/**
-     * IDを指定して書籍を1件取得します。
-     * 見つからない場合は null を返すか、例外を投げます。
-     */
-	@Transactional(readOnly=true)
-	@Cacheable(value="getBook",key="#p0")
+	 * IDを指定して書籍を1件取得します。
+	 * 見つからない場合は null を返すか、例外を投げます。
+	 */
+	@Transactional(readOnly = true)
+	@Cacheable(value = "getBook", key = "#p0")
 	public Book findById(Long id) {
+
+		//キャッシュが機能しているかの確認
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+
 		return bookRepository.findById(id).orElse(null);
 	}
-	
+
 	/**
-     * 書籍を保存（新規登録・更新）します。
-     * IDがあれば更新、なければ新規登録として動きます。
-     */
+	 * 書籍を保存（新規登録・更新）します。
+	 * IDがあれば更新、なければ新規登録として動きます。
+	 */
 	@Transactional
-	@CacheEvict(value="getBooks",allEntries=true)
+	@CacheEvict(value = "getBooks", allEntries = true)
 	public void save(Book book) {
 		bookRepository.save(book);
 	}
-	
+
 	/**
-     * IDを指定して書籍を削除します。
-     */
+	 * IDを指定して書籍を削除します。
+	 */
 	@Transactional
 	@Caching(evict = {
-			@CacheEvict(value="getBook",key="#p0"),
-			@CacheEvict(value="getBooks",allEntries=true)
+			@CacheEvict(value = "getBook", key = "#p0"),
+			@CacheEvict(value = "getBooks", allEntries = true)
 	})
 	public void deleteById(Long id) {
+
+		//キャッシュが機能しているかの確認
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+
 		bookRepository.deleteById(id);
 	}
 }
