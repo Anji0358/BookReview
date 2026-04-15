111f51324c56cf81c7ec199cd3a858b30e9ae268
Anji <zam58mrp@i.softbank.jp>
2026-04-15 21:32:00 +0900

    feat: implement signup validation

--- src/main/java/com/example/bookreview/form/SignupForm.java
+++ src/main/java/com/example/bookreview/form/SignupForm.java
@@ -1,5 +1,20 @@
 package com.example.bookreview.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
 public class SignupForm {
 
+	@NotBlank(message = "ユーザー名を入力してください")
+    private String username;
+
+    @NotBlank(message = "メールアドレスを入力してください")
+    @Email(message = "メール形式で入力してください")
+    private String email;
+
+    @NotBlank(message = "パスワードを入力してください")
+    private String password;
 }
