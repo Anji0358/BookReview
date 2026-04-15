c705f349f251b15b9353ab7d2d551dee87e95241
Anji <zam58mrp@i.softbank.jp>
2026-04-15 19:38:10 +0900

    add: SequrityConfigの追加

---
+++ src/main/java/com/example/bookreview/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.bookreview.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.bookreview.service.CustomUserDetailsService;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	private final CustomUserDetailsService customUserDetailsService;
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+	    DaoAuthenticationProvider authProvider =
+	            new DaoAuthenticationProvider(customUserDetailsService);
+	    authProvider.setPasswordEncoder(passwordEncoder());
+	    return authProvider;
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authenticationProvider(authenticationProvider())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/", "/books", "/books/*", "/register", "/login", "/css/**").permitAll()
+						.requestMatchers("/admin/**").hasRole("ADMIN")
+						.anyRequest().authenticated())
+				.formLogin(form -> form
+						.loginPage("/login")
+						.defaultSuccessUrl("/books", true)
+						.permitAll())
+				.logout(logout -> logout
+						.logoutSuccessUrl("/books")
+						.permitAll());
+
+		return http.build();
+	}
+}
