267343d0ad477c2ab2201853ef2ac0519e192003
Anji <zam58mrp@i.softbank.jp>
2026-04-15 21:33:23 +0900

    feat: implement login validation

--- src/main/java/com/example/bookreview/form/LoginForm.java
+++ src/main/java/com/example/bookreview/form/LoginForm.java
@@ -1,5 +1,15 @@
 package com.example.bookreview.form;
 
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Data;
+
+@Data
 public class LoginForm {
 
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
 }
