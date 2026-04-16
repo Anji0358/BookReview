c2209aa00c416f002c8891bfaa859dec29409f2e
Anji <zam58mrp@i.softbank.jp>
2026-04-16 16:01:24 +0900

    feat: add CustomUserDetailsService class

---
+++ src/main/java/com/example/bookreview/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.bookreview.security;
+
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.bookreview.entity.User;
+import com.example.bookreview.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("ユーザーが見つかりません"));
+
+        return new LoginUser(
+                user,
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+    }
+}
