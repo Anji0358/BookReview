d9354a288a10064acd8b9e3ab122415821f5ab4b
Anji <zam58mrp@i.softbank.jp>
2026-04-17 14:45:01 +0900

    feat:add validations to SignupForm

--- src/main/java/com/example/bookreview/form/SignupForm.java
+++ src/main/java/com/example/bookreview/form/SignupForm.java
@@ -2,6 +2,7 @@ package com.example.bookreview.form;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import lombok.Data;
 public class SignupForm {
 
 	@NotBlank(message = "ユーザー名を入力してください")
+	@Size(min = 3, max = 20, message = "ユーザー名は3〜20文字で入力してください")
     private String username;
 
     @NotBlank(message = "メールアドレスを入力してください")
@@ -16,5 +18,6 @@ public class SignupForm {
     private String email;
 
     @NotBlank(message = "パスワードを入力してください")
+    @Size(min = 8, message = "パスワードは8文字以上で入力してください")
     private String password;
 }
